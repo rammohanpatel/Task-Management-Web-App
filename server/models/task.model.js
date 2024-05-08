660b2671f4c48db589eee52b2512589cfda76397
Ram Mohan Patel <103207998+rammohanpatel@users.noreply.github.com>
2024-05-08 17:35:48 +0530

    CRUD application added

--- server/models/task.model.js
+++ server/models/task.model.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 const taskSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   task: { type: String, required: true },
-  priority: { type: String, enum: ['high', 'medium', 'low'], default: 'medium' },
+  priority: { type: String, enum: ['High', 'Medium', 'Low'], default: 'Medium' },
   dueDate: { type: String, required: true},
   completed: { type: Boolean, default: false },
   createdAt: { type: Date, default: Date.now },
