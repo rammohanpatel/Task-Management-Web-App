c66a076c5f9dd9848721168c250032f91686d404
Vinit Patidar <Vinitpatidar780@gmail.com>
2024-05-08 18:13:07 +0530

    .

--- server/index.js
+++ server/index.js
@@ -3,29 +3,28 @@ const app = express();
 import cors from 'cors';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
-import bodyParser from 'body-parser';
 import taskRoutes from './routes/tasks.js';
 import userRoutes from './routes/user.js';
 
 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cors())
 app.use(express.json());
 
 dotenv.config();
 
-const PORT=process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(taskRoutes);
 app.use(userRoutes);
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('Hello to Task Manager API')
 })
 
 mongoose.connect(process.env.CONNECTION_URL)
-.then(()=>{
-    app.listen(PORT,()=> console.log(`Server running on port : ${PORT}`))
-})
-.catch((error)=>console.log(error.message))
+    .then(() => {
+        app.listen(PORT, () => console.log(`Server running on port : ${PORT}`))
+    })
+    .catch((error) => console.log(error.message))
 
