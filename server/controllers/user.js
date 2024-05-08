6fea663b1d876f9ed3f5daf2813eeccf80972c45
Vinit Patidar <Vinitpatidar780@gmail.com>
2024-05-08 07:43:33 +0530

    .

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -6,33 +6,33 @@ import User from "../models/user.model.js";
 
 const secret = 'taskmanagementsystem';
 export const signin = async (req, res) => {
-  const { email, password } = req.body;
+    const { email, password } = req.body;
 
-  try {
-    const existingUser = await User.findOne ({ email });
-    if(!existingUser) return res.status(404).json({message:"User doesn't exist"});
-    const isPasswordCorrect = await bcrypt.compare(password, existingUser.password);
-    if(!isPasswordCorrect) return res.status(400).json({message:"Invalid credentials"});
-    const token = jwt.sign({email:existingUser.email,id:existingUser._id},secret,{expiresIn:'1h'});
-    res.status(200).json({result:existingUser,token});
+    try {
+        const existingUser = await User.findOne({ email });
+        if (!existingUser) return res.status(404).json({ message: "User doesn't exist" });
+        const isPasswordCorrect = await bcrypt.compare(password, existingUser.password);
+        if (!isPasswordCorrect) return res.status(400).json({ message: "Invalid credentials" });
+        const token = jwt.sign({ email: existingUser.email, id: existingUser._id }, secret, { expiresIn: '1h' });
+        res.status(200).json({ success: true, token });
     }
     catch (error) {
-        res.status(500).json({message:'Something went wrong'});
-        }
+        res.status(500).json({ message: 'Something went wrong' });
     }
+}
 
 export const signup = async (req, res) => {
-    const {username, email, password} = req.body;
+    const { username, email, password } = req.body;
     try {
-        const existing  = await User.findOne({email});
-        if(existing) return res.status(400).json({message:'User already exists'});
-        const hashedPassword = await bcrypt.hash(password,12);
-        const result = await User.create({email,password:hashedPassword,username});
-        const token = jwt.sign({email:result.email,id:result._id},secret,{expiresIn:'1h'});
-        res.status(200).json({success:true,token});
+        const existing = await User.findOne({ email });
+        if (existing) return res.status(400).json({ message: 'User already exists' });
+        const hashedPassword = await bcrypt.hash(password, 12);
+        const result = await User.create({ email, password: hashedPassword, username });
+        const token = jwt.sign({ email: result.email, id: result._id }, secret, { expiresIn: '1h' });
+        res.status(200).json({ success: true, token });
     }
     catch (error) {
-        res.status(500).json({message:'Something went wrong'});
+        res.status(500).json({ message: 'Something went wrong' });
     }
 }
 
--- server/middleware/checkUser.js
+++ server/middleware/checkUser.js
@@ -2,16 +2,15 @@ import jwt from 'jsonwebtoken';
 
 const secret = 'taskmanagementsystem';
 
-const checkUser = (req,res,next)=>{
-    const token  = req.headers.authToken;
-    if(token){
-        jwt.verify(token,secret,(err,decodedToken)=>{
-            if(err){
-                res.status(401).json({message:'Unauthorized'});
+const checkUser = (req, res, next) => {
+    const token = req.headers.authtoken;
+    if (token) {
+        jwt.verify(token, secret, (err, decodedToken) => {
+            if (err) {
+                res.status(401).json({ message: 'Unauthorized' });
             }
-            else{
-                console.log(decodedToken)
-                req.body = decodedToken;
+            else {
+                req.body.decodedToken = decodedToken;
                 next();
             }
         })
